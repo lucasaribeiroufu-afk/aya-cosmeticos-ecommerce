d255d6d5913d9f8624a004efa6fc5ce95c930e3b
lucasaribeiroufu-afk <lucas.aribeiro.ufu@gmail.com>
2026-08-12 16:01:36 -0300

    Update vite.config.js

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import React from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [React()],
   resolve: {
     alias: {
       '@': '/src',
