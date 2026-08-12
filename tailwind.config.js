d7dc15847fcbad44f1b2d3c46f7f0f251dc2c108
lucasaribeiroufu-afk <lucas.aribeiro.ufu@gmail.com>
2026-08-12 16:25:03 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import tailwindcssAnimate from "tailwindcss-animate";
+
 /** @type {import('tailwindcss').Config} */
 export default {
     darkMode: ["class"],
@@ -83,5 +85,5 @@ export default {
   		}
   	}
   },
-  plugins: [await import("tailwindcss-animate").then(m => m.default)],
+  plugins: [tailwindcssAnimate],
 }
